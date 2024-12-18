b7c44feaf8e0b5bd48949a724a504a640818e42f
Alberto Pardo Díaz <albertopardo21690@gmail.com>
2024-12-18 17:34:38 +0100

    first commit

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*=============================================
+Depurar Errores
+=============================================*/
+
+define('DIR',__DIR__);
+
+ini_set("display_errors", 1);
+ini_set("log_erros", 1);
+ini_set("error_log", DIR."/php_error_log");
+
+require_once "controllers/template.controller.php";
+
+$index = new TemplateController();
+$index->index();
